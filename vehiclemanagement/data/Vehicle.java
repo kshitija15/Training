e262ee19cf3578d9139e049ed2764a26fbc2614d
kshitija15 <52289692+kshitija15@users.noreply.github.com>
2022-02-03 13:27:14 +0530

    Add files via upload

---
+++ vehiclemanagement/data/Vehicle.java
@@ -0,0 +1,35 @@
+package io.sevenx.vehiclemanagement.data;
+
+public class Vehicle {
+	private int vehicleNumber;
+	private String fuelType;
+	private String Colour;
+	//private int cost;
+	/*public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}*/
+	public int getVehicleNumber() {
+		return vehicleNumber;
+	}
+	public void setVehicleNumber(int vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+	public String getFuelType() {
+		return fuelType;
+	}
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+	public String getColour() {
+		return Colour;
+	}
+	public void setColour(String colour) {
+		Colour = colour;
+	}
+	
+	
+
+}
