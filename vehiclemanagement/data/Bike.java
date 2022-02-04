a146c43d9526fe13416db9aea6075687f4377ca4
kshitija15 <52289692+kshitija15@users.noreply.github.com>
2022-02-04 09:05:17 +0530

    Add files via upload

---
+++ vehiclemanagement/data/Bike.java
@@ -0,0 +1,35 @@
+package io.sevenx.vehiclemanagement.data;
+
+public class Bike extends Vehicle {
+ /*private String BikeName;
+ private int BikeCapacity;
+ private double BikePrice;
+public String getBikeName() {
+	return BikeName;
+}
+public void setBikeName(String bikeName) {
+	BikeName = bikeName;
+}
+public int getCapacity() {
+	return BikeCapacity;
+}
+public void setCapacity(int bikeCapacity) {
+	BikeCapacity = bikeCapacity;
+}
+public double getBikePrice() {
+	return BikePrice;
+}
+public void setBikePrice(double bikePrice) {
+	BikePrice = bikePrice;
+}*/
+	private int power;
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+	
+}
---
+++ vehiclemanagement/data/Vehicle.java
@@ -0,0 +1,61 @@
+package io.sevenx.vehiclemanagement.data;
+
+public class Vehicle {
+	//private int vehicleNumber;
+	private String fuelType;
+	private String Colour;
+	private String brand;
+	private int  maximumSpeed;
+	private int numberOfGears;
+	
+
+	
+	
+	//private int cost;
+	/*public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}*/
+	/*public int getVehicleNumber() {
+		return vehicleNumber;
+	}
+	public void setVehicleNumber(int vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}*/
+	public String getFuelType() {
+		return fuelType;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public int getMaximumSpeed() {
+		return maximumSpeed;
+	}
+	public void setMaximumSpeed(int maximumSpeed) {
+		this.maximumSpeed = maximumSpeed;
+	}
+	public int getNumberOfGears() {
+		return numberOfGears;
+	}
+	public void setNumberOfGears(int numberOfGears) {
+		this.numberOfGears = numberOfGears;
+	}
+	
+	public void setFuelType(String fuelType) {
+		this.fuelType = fuelType;
+	}
+	public String getColour() {
+		return Colour;
+	}
+	public void setColour(String colour) {
+		Colour = colour;
+	}
+	
+	
+
+}
